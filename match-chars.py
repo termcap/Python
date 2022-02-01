6fb73296b39e4e137b467c7764fffb9cf3526091
termcap <27614065+termcap@users.noreply.github.com>
2022-02-01 19:34:09 +0300

    Update match-chars.py
    More options

--- match-chars.py
+++ match-chars.py
@@ -1,3 +1,10 @@
 import re
-print(bool(re.match('^[a-zA-Z0-9.\-_()\\\]+$', r'\2022\01\path-to_file(GRE)-v.1')))
-print(bool(re.match('^[1234]+$', '123')))
+#print(bool(re.match('^[a-zA-Z0-9.\-_()\\\]+$', r'\2022\01\path-to_file(GRE)-v.1')))
+#print(bool(re.match('^[1234]+$', '123')))
+text = r'\2022\01\path-to_fileGRE--v.1'
+pattern = re.compile('^[a-zA-Z0-9.\-_()\\\]+$')
+matches = pattern.findall(text)
+
+print(matches)
+if(matches):
+    print ("match found")
